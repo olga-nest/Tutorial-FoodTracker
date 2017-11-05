da26f00598f31d6de6842149f78e4b99cbbe33d6
olga-nest <olga.w.nesterova@gmail.com>
2017-11-04 19:35:51 -0700

    Add star button images

---
+++ TutorialFoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  TutorialFoodTracker
+//
+//  Created by Olga on 11/4/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
