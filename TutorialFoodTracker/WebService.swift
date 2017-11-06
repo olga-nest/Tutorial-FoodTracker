d56e6746921ded911d9af2e5f26c6273209786c2
olga-nest <olga.w.nesterova@gmail.com>
2017-11-06 15:51:38 -0800

    Add webService class

---
+++ TutorialFoodTracker/WebService.swift
@@ -0,0 +1,17 @@
+//
+//  WebService.swift
+//  TutorialFoodTracker
+//
+//  Created by Olga on 11/6/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+import Foundation
+
+class WebService {
+    
+    //MARK: Properties
+    static let shared = WebService()
+    
+    
+}
