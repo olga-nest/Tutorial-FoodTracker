da0e4d96f97a04e8aba9dc256406e2ad096431c9
olga-nest <olga.w.nesterova@gmail.com>
2017-11-06 18:54:04 -0800

    Signup with user's provided data

--- TutorialFoodTracker/SignupViewController.swift
+++ TutorialFoodTracker/SignupViewController.swift
@@ -18,45 +18,11 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    func saveUserLoginData() {
-        var username: String
-        var password: String
-        if !(usernameTextField.text?.isEmpty)! && !(passwordTextField.text?.isEmpty)! {
-            username = usernameTextField.text!
-            password = passwordTextField.text!
-            
-            UserDefaults.standard.set(username, forKey: "username")
-            UserDefaults.standard.set(password, forKey: "password")
-            
-            usernameTextField.text = ""
-            passwordTextField.text = ""
-        }
-       
     }
     
     //MARK: Actions
     @IBAction func SignupButtonTapped(_ sender: UIButton) {
-        saveUserLoginData()
+        WebService.shared.signUp(with: (usernameTextField.text)!, password: (passwordTextField.text)!, and: self)
     }
-
+    
 }
