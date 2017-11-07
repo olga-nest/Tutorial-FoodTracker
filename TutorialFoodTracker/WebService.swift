5f2000712844a312d44cb86812fa82ef92abc2e6
olga-nest <olga.w.nesterova@gmail.com>
2017-11-06 18:54:57 -0800

    Sign up for account

--- TutorialFoodTracker/WebService.swift
+++ TutorialFoodTracker/WebService.swift
@@ -7,11 +7,60 @@
 //
 
 import Foundation
+import UIKit
 
 class WebService {
     
     //MARK: Properties
     static let shared = WebService()
     
-    
+    // MARK: - Networking
+    func signUp(with email: String, password: String, and presentingViewController: UIViewController) {
+        
+        let postData = [
+        "username": email,
+        "password": password ]
+
+        guard let postJSON = try? JSONSerialization.data(withJSONObject: postData, options: []) else {
+            print("could not serialize json")
+            return
+        }
+        let url = URL(string: "https://cloud-tracker.herokuapp.com/signup")!
+        let request = NSMutableURLRequest(url: url)
+        request.httpBody = postJSON
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            guard let data = data else {
+                print("no data returned from server \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                print("no response returned from server \(String(describing: error))")
+                return
+            }
+            
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as! [String:Any] else {
+                print("data returned is not json, or not valid")
+                return
+            }
+            
+            let user = User()
+            user.parse(with: json)
+            
+            presentingViewController.dismiss(animated: true, completion: nil)
+            
+            guard response.statusCode == 200 else {
+                // handle error
+                print("an error occurred \(String(describing: json["error"]))")
+                return
+            }
+          
+            }
+        // do something with the json object
+        task.resume()
+    }
 }
+
+
